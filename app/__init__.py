290f065bcbc0ce26fe46c11bffacbd910d2abf0d
MajorRedbeard <ryan.lindsey@gmail.com>
2024-12-25 14:03:59 -0500

    Added base.html and added to init.py

--- app/__init__.py
+++ app/__init__.py
@@ -9,10 +9,14 @@ def create_app():
     from .routes.services import services
     from .routes.ports import ports
     from .routes.nginx import nginx
+	from .routes.settings import settings_bp
+
 
     app.register_blueprint(main)
     app.register_blueprint(services, url_prefix='/services')
     app.register_blueprint(ports, url_prefix='/ports')
     app.register_blueprint(nginx, url_prefix='/nginx')
+	app.register_blueprint(settings_bp, url_prefix='/settings')
+
 
     return app
